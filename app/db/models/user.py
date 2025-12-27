d4b6fa71d0c56aa8c03695c7715df7ce3ae198e8
Kirill Voroshilov <oroshilov.cyril@yandex.ru>
2025-12-27 04:49:25 +0300

    edit user db

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -9,8 +9,6 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
-    password_hash: Mapped[str]
     name: Mapped[str | None]
     created_at: Mapped[datetime] = mapped_column(default=datetime.now)
 
--- app/domain/entities/user.py
+++ app/domain/entities/user.py
@@ -5,8 +5,6 @@ from app.domain.enum import UserRolle
 @dataclass
 class User: 
     id: int
-    email: str
-    password_hash: str
     name: str | None
     created_at: datetime
     roles: UserRolle
